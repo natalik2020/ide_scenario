43c6949cb4fc98e093647885d72c3f7c6bc863d5
geekbrains <natalik_1991@mail.ru>
2021-08-26 15:39:32 +0300

    lesson 3

---
+++ src/main/java/belovaTestScenario1/LiveLibTest.java
@@ -0,0 +1,40 @@
+package belovaTestScenario1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class LiveLibTest {
+    private static WebDriver driver;
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+
+        //scenarioWithExtention();
+
+        driver = new ChromeDriver();
+        //runJsScriptExample();
+
+        driver.manage().window().maximize();
+        WebDriverWait webDriverWait = new WebDriverWait(driver, 5);
+
+        loginToCrm();
+
+    }
+
+    private static void loginToCrm() {
+    }
+}
---
+++ src/main/java/belovaTestScenario1/SetupBrowserExample.java
@@ -0,0 +1,28 @@
+package belovaTestScenario1;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class SetupBrowserExample {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions
+                .addArguments("--disable-notifications")
+                .addArguments("user-agent=Googlebot/2.1 (+http://www.google.com/bot.html)");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://google.com");
+
+        driver.manage().window().maximize();
+        Thread.sleep(5000);
+        driver.manage().window().setSize(new Dimension(500, 500));
+        Thread.sleep(5000);
+        driver.quit();
+    }
+}
